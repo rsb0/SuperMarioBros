8b45f6f8dbe1afa8fb337de86bfedba46d336a0f
rsb0 <rune.s.brekke@gmail.com>
2019-03-11 21:08:07 +0100

    first commit

---
+++ core/src/com/runesb/mariobros/MarioBros.java
@@ -0,0 +1,31 @@
+package com.runesb.mariobros;
+
+import com.badlogic.gdx.Game;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.runesb.mariobros.Screens.PlayScreen;
+
+public class MarioBros extends Game {
+
+    public static final int V_WIIDTH = 400;
+    public static final int V_HEIGHT = 208;
+
+	public SpriteBatch batch;
+
+
+	@Override
+	public void create () {
+		batch = new SpriteBatch();
+		setScreen(new PlayScreen(this));
+
+	}
+
+	@Override
+	public void render () {
+		super.render();
+	}
+	
+	@Override
+	public void dispose () {
+		batch.dispose();
+	}
+}
---
+++ core/src/com/runesb/mariobros/Screens/PlayScreen.java
@@ -0,0 +1,101 @@
+package com.runesb.mariobros.Screens;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.Screen;
+import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.OrthographicCamera;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.maps.tiled.TiledMap;
+import com.badlogic.gdx.maps.tiled.TmxMapLoader;
+import com.badlogic.gdx.maps.tiled.renderers.OrthogonalTiledMapRenderer;
+import com.badlogic.gdx.utils.viewport.FitViewport;
+import com.badlogic.gdx.utils.viewport.Viewport;
+import com.runesb.mariobros.MarioBros;
+import com.runesb.mariobros.Scenes.Hud;
+
+public class PlayScreen implements Screen {
+
+    private MarioBros game;
+    private Texture texture;
+
+    private OrthographicCamera gameCam;
+    private Viewport gamePort;
+    private Hud hud;
+
+    private TmxMapLoader mapLoader;
+    private TiledMap map;
+    private OrthogonalTiledMapRenderer renderer;
+
+
+    public PlayScreen(MarioBros game){
+        this.game = game;
+
+        gameCam = new OrthographicCamera();
+        gamePort = new FitViewport(MarioBros.V_WIIDTH, MarioBros.V_HEIGHT, gameCam);
+        hud = new Hud(game.batch);
+
+        mapLoader = new TmxMapLoader();
+        map = mapLoader.load("level1.tmx");
+        renderer = new OrthogonalTiledMapRenderer(map);
+
+        gameCam.position.set(gamePort.getWorldWidth() / 2, gamePort.getWorldHeight() / 2, 0);
+
+    }
+
+    @Override
+    public void show() {
+
+    }
+
+    public void handleInput(float dt){
+        if(Gdx.input.isTouched()){
+            gameCam.position.x += 100 * dt;
+        }
+
+    }
+
+    public void update(float dt){
+        handleInput(dt);
+        gameCam.update();
+        renderer.setView(gameCam);
+    }
+
+    @Override
+    public void render(float delta) {
+        update(delta);
+
+        //clear screen with red
+        Gdx.gl.glClearColor(1,0,0,1);
+        Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+
+        renderer.render();
+
+        game.batch.setProjectionMatrix(hud.stage.getCamera().combined);
+        hud.stage.draw();
+    }
+
+    @Override
+    public void resize(int width, int height) {
+        gamePort.update(width,height);
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
